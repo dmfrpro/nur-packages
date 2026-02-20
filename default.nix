569dae809cc052c218a6065779ad33a10e1f4244
dmfrpro <dmfr2021y@gmail.com>
2026-02-20 20:52:36 +0300

    pkgs: delete nix-plugins
    Signed-off-by: dmfrpro <dmfr2021y@gmail.com>

--- default.nix
+++ default.nix
@@ -25,8 +25,6 @@
   auroraos-asbt-apptool = pkgs.callPackage ./pkgs/auroraos-asbt-apptool { };
   auroraos-platform-sdk = pkgs.callPackage ./pkgs/auroraos-platform-sdk { };
 
-  nix-plugins = pkgs.callPackage ./pkgs/nix-plugins { };
-
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
